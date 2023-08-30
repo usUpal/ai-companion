dea15fd34cd487e4d006300aea22f6f2738a32eb
usupal <udyan.upal@gmail.com>
2023-08-30 16:11:51 +0600

    🔧 fix(layout.tsx): add Navbar component to the RootLayout to display a navigation bar ✨ feat(navbar.tsx): create a new Navbar component to display a fixed navigation bar with a logo and menu icon 🔧 fix(package.json): update dependencies and add postinstall script to generate Prisma client after installation

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+
+export const Navbar = () => {
+  return (
+    <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary'>
+      <div className='flex items-center'>
+        <Menu className="block md:hidden" />
+        <Link href='/'>
+            <h1 className="hidden md:block">companion.ai</h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
